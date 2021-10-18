fa144a66a917a4fe34e517a1377981591248ce80
bwaah1 <chykivan@gmail.com>
2021-10-18 23:14:44 +0300

    second test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double h;
+			double x = 2;
+			double y = 0;
+		
+		    h = (pow(x, 2) + pow(y, 2)) / (1 + pow(x, 2) + pow(x, 2) * pow(y, 2));
+			Assert::AreEqual(h, 0,8);
+		}
+	};
+}
